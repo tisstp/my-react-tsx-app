198679f63b2850db88fbc9f32e095cea55b1fbaf
sathaphorn.stp <sathaphorn.stp@gmail.com>
2020-04-06 17:13:53 +0700

    material-ui: add Header

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Navbar from './Navbar';
+import Header from './Header';
+
+const useStyles = makeStyles({});
 
 const Home = () => {
+  const classes = useStyles();
+
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+      <Header />
+    </>
   );
 };
 
